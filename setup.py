0bb1b7d0e13149decb6014318faa67f59260fb86
Edward Kim <onward.edkim@gmail.com>
2016-09-16 15:50:58 +1000

    add classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,22 @@ setup(
         'pytest-runner'
     ],
     tests_require=[
-        'pytest',
+        'pytest'
     ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
+
