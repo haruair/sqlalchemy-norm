4f21743ade3332779d07347e1bcd6a05b5a05616
Hupyeong Lee <etermory@gmail.com>
2016-09-20 13:09:15 +0900

    pythonic diet

--- sqlalchemy_norm/parser.py
+++ sqlalchemy_norm/parser.py
@@ -2,22 +2,17 @@ def parse(items):
     items = set(items)
 
     digested = [item.split('.') for item in items]
-    properties = [item[0] for item in digested if len(item) == 1]
-
     legacy = dict()
 
     for item in digested:
         if len(item) > 1:
-            if item[0] not in legacy:
-                legacy[item[0]] = set()
             prop = item[1:] if len(item) < 2 else '.'.join(item[1:])
-            legacy[item[0]].add(prop)
+            legacy_item = legacy.setdefault(item[0], set())
+            legacy_item.add(prop)
 
     result = {
-        'property': set(properties)
+        'property': set(item[0] for item in digested if len(item) == 1),
+        'legacy': legacy
     }
-
-    if len(legacy) > 0:
-        result['legacy'] = legacy
 
     return result
